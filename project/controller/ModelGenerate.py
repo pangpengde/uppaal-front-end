dc58611dead35da951234b2b608a5f584840febd
pangpengde <pangpengde@gmail.com>
2014-11-23 16:34:56 +0800

    test UserInput

---
+++ project/controller/ModelGenerate.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from model import UserInput
+
+class ModelGenerate():
+    """docstring for ModelGenerate"""
+    def __init__(self, arg):
+        pass
+
+
+
+if __name__ == '__main__':
+   ui = UserInput() 
+   ui.test()
+   
--- project/model/UserInput.py
+++ project/model/UserInput.py
@@ -10,3 +10,11 @@ class UserInput(object):
 
     def __init__(self):
         pass
+
+    def test():
+        t1 = Task()
+        tasks.append(t1)
+        t2 = Task()
+        # TODO 把task里的变量都赋值上 
+
+        tasks.append(t2)
