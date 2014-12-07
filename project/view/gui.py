5550a103e99dc4b70450500515858b30e49b6f5b
touyo <touyo.theone@gmail.com>
2014-12-07 20:44:04 +0800

    task panel

--- project/view/gui.py
+++ project/view/gui.py
@@ -31,12 +31,54 @@ class mainFrame(wx.Frame):
 
     def init_panel(self):
         mainSizer = wx.BoxSizer(wx.VERTICAL)
+
+        specSizer = wx.BoxSizer(wx.VERTICAL)
         specification = wx.Panel(self, -1)
-        wx.TextCtrl(specification, pos=(3, 3), size=(775, 250))
+        taskGrid = wx.GridBagSizer(hgap=7, vgap=2)
+
+        name = wx.StaticText(self, label="task name: ", pos=(10, 10))
+        taskGrid.Add(name, pos=(0, 0))
+
+        editname = wx.TextCtrl(self, pos=(10, 60), size=(70, -1))
+        taskGrid.Add(editname, pos=(1, 0))
+
+        period = wx.StaticText(self, label="period: ", pos=(40, 10))
+        taskGrid.Add(period, pos=(0, 1))
+
+        editperiod = wx.TextCtrl(self, pos=(40, 60), size=(70, -1))
+        taskGrid.Add(editperiod, pos=(1, 1))
+
+        offset = wx.StaticText(self, label="offset: ", pos=(70, 10))
+        taskGrid.Add(offset, pos=(0, 2))
+
+        editoffset= wx.TextCtrl(self, pos=(70, 60), size=(70, -1))
+        taskGrid.Add(editoffset, pos=(1, 2))
+
+        bect = wx.StaticText(self, label="bect: ", pos=(100, 10))
+        taskGrid.Add(bect, pos=(0, 3))
+
+        editbect = wx.TextCtrl(self, pos=(100, 60), size=(70, -1))
+        taskGrid.Add(editbect, pos=(1, 3))
+
+        wect = wx.StaticText(self, label="wect: ", pos=(130, 10))
+        taskGrid.Add(wect, pos=(0, 4))
+
+        editwect = wx.TextCtrl(self, pos=(130, 60), size=(70, -1))
+        taskGrid.Add(editwect, pos=(1, 4))
+
+        minusButton = wx.Button(self, label="-", pos=(160, 60))
+        taskGrid.Add(minusButton, pos=(1, 5))
+        plusButton = wx.Button(self, label="+", pos=(190, 60))
+        taskGrid.Add(plusButton, pos=(1, 6))
+
+        #wx.TextCtrl(specification, pos=(3, 3), size=(775, 250))
+
+        specSizer.Add(taskGrid, 0, wx.LEFT)
 
         result = wx.Panel(self, -1)
         wx.TextCtrl(result, pos=(3, -1), size = (775, 250))
-        mainSizer.Add(specification, 0, wx.CENTER)
+
+        mainSizer.Add(specSizer, 0, wx.CENTER)
         mainSizer.Add(result, 0, wx.CENTER)
         self.SetSizerAndFit(mainSizer)
         #self.add_task()
