5e2b8e42239283ab08d06e3126100a00c2f193b7
touyo <touyo.theone@gmail.com>
2014-12-21 20:56:21 +0800

    gui

--- project/view/gui.py
+++ project/view/gui.py
@@ -5,9 +5,10 @@ import wx
 class mainFrame(wx.Frame):
 
     def __init__(self, parent, title):
-        super(mainFrame, self).__init__(parent, title=title, size=(600, 600))
+        super(mainFrame, self).__init__(parent, title=title, size=(900, 600))
         self.init_toolbar()
-        self.init_panel()
+        self.init_specpanel()
+        # self.init_resultpenal()
 
     def init_toolbar(self):
         menubar = wx.MenuBar()
@@ -18,174 +19,156 @@ class mainFrame(wx.Frame):
         fileMenu.AppendSeparator()
         fitem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit Tool')
         menubar.Append(fileMenu, '&File')
-
         aboutmenu = wx.Menu()
         aboutmenu.Append(wx.ID_ABOUT, '&About', 'About this tool')
         menubar.Append(aboutmenu, '&About')
-
         self.SetMenuBar(menubar)
-
         self.Bind(wx.EVT_MENU, self.on_quit, fitem)
 
     def on_quit(self, e):
         self.Close()
 
-    def init_panel(self):
-        mainSizer = wx.BoxSizer(wx.VERTICAL)
-
+    def init_specpanel(self):
+        specpanel = wx.Panel(self)
         specSizer = wx.BoxSizer(wx.VERTICAL)
-        # specification = wx.Panel(self, -1)
-        # wx.TextCtrl(specification, pos=(3, 3), size=(775, 250))
 
         # Task
-        taskGrid = wx.GridBagSizer(hgap=7, vgap=3)
+        taskGrid = wx.GridBagSizer(hgap=10, vgap=3)
 
-        task = wx.StaticText(self, label="Task: ", pos=(0, 10))
+        task = wx.StaticText(specpanel, label="Task: ")
         taskGrid.Add(task, pos=(0, 0))
 
-        tname = wx.StaticText(self, label="task name: ", pos=(10, 10))
+        tname = wx.StaticText(specpanel, label="task name: ")
         taskGrid.Add(tname, pos=(1, 0))
 
-        edit_tname = wx.TextCtrl(self, pos=(10, 60), size=(70, -1))
+        edit_tname = wx.TextCtrl(specpanel, size=(70, -1))
         taskGrid.Add(edit_tname, pos=(2, 0))
 
-        period = wx.StaticText(self, label="period: ", pos=(40, 10))
-        taskGrid.Add(period, pos=(1, 1))
+        offset = wx.StaticText(specpanel, label="offset: ")
+        taskGrid.Add(offset, pos=(1, 1))
 
-        editperiod = wx.TextCtrl(self, pos=(40, 60), size=(70, -1))
-        taskGrid.Add(editperiod, pos=(2, 1))
+        editoffset= wx.TextCtrl(specpanel, size=(70, -1))
+        taskGrid.Add(editoffset, pos=(2, 1))
 
-        offset = wx.StaticText(self, label="offset: ", pos=(70, 10))
-        taskGrid.Add(offset, pos=(1, 2))
+        task_bcet = wx.StaticText(specpanel, label="bect: ")
+        taskGrid.Add(task_bcet, pos=(1, 2))
 
-        editoffset= wx.TextCtrl(self, pos=(70, 60), size=(70, -1))
-        taskGrid.Add(editoffset, pos=(2, 2))
+        edit_task_bcet = wx.TextCtrl(specpanel,  size=(70, -1))
+        taskGrid.Add(edit_task_bcet, pos=(2, 2))
 
-        bect = wx.StaticText(self, label="bect: ", pos=(100, 10))
-        taskGrid.Add(bect, pos=(1, 3))
+        task_wcet = wx.StaticText(specpanel, label="wect: ")
+        taskGrid.Add(task_wcet, pos=(1, 3))
 
-        editbect = wx.TextCtrl(self, pos=(100, 60), size=(70, -1))
-        taskGrid.Add(editbect, pos=(2, 3))
+        edit_task_wcet = wx.TextCtrl(specpanel, size=(70, -1))
+        taskGrid.Add(edit_task_wcet, pos=(2, 3))
 
-        wect = wx.StaticText(self, label="wect: ", pos=(130, 10))
-        taskGrid.Add(wect, pos=(1, 4))
+        deadline = wx.StaticText(specpanel, label="deadline: ")
+        taskGrid.Add(deadline, pos=(1, 4))
 
-        editwect = wx.TextCtrl(self, pos=(130, 60), size=(70, -1))
-        taskGrid.Add(editwect, pos=(2, 4))
+        edit_deadline = wx.TextCtrl(specpanel, size=(70, -1))
+        taskGrid.Add(edit_deadline, pos=(2, 4))
 
-        minusButton = wx.Button(self, label="-")
-        taskGrid.Add(minusButton, pos=(2, 5))
-        plusButton = wx.Button(self, label="+")
-        taskGrid.Add(plusButton, pos=(2, 6))
+        period = wx.StaticText(specpanel, label="period: ")
+        taskGrid.Add(period, pos=(1, 5))
 
-        # Platform
-        platformGrid = wx.GridBagSizer(hgap=5, vgap=3)
+        editperiod = wx.TextCtrl(specpanel, size=(70, -1))
+        taskGrid.Add(editperiod, pos=(2, 5))
 
-        platform = wx.StaticText(self, label="Platform: ", pos=(0, 10))
-        platformGrid.Add(platform, pos=(0, 0))
+        processor = wx.StaticText(specpanel, label="processor: ")
+        taskGrid.Add(processor, pos=(1, 6))
 
-        pname = wx.StaticText(self, label="pf name: ", pos=(10, 10))
-        platformGrid.Add(pname, pos=(1, 0))
+        editprocessor = wx.TextCtrl(specpanel, size=(70, -1))
+        taskGrid.Add(editprocessor, pos=(2, 6))
 
-        edit_pname = wx.TextCtrl(self, pos=(10, 60), size=(70, -1))
-        platformGrid.Add(edit_pname, pos=(2, 0))
+        predecessor = wx.StaticText(specpanel, label="predecessor: ")
+        taskGrid.Add(predecessor, pos=(1, 7))
 
-        strategy = wx.StaticText(self, label="strategy: ", pos=(40, 10))
-        platformGrid.Add(strategy, pos=(1, 1))
+        editpredecessor = wx.TextCtrl(specpanel, size=(70, -1))
+        taskGrid.Add(editpredecessor, pos=(2, 7))
 
-        editstrategy = wx.TextCtrl(self, pos=(40, 60), size=(70, -1))
-        platformGrid.Add(editstrategy, pos=(2, 1))
+        task_dButton = wx.Button(specpanel,  size=(70, -1), label="Delete")
+        taskGrid.Add(task_dButton, pos=(2, 8))
+        task_aButton = wx.Button(specpanel,  size=(70, -1), label="Add")
+        taskGrid.Add(task_aButton, pos=(2, 9))
 
-        speed = wx.StaticText(self, label="speed(bus): ", pos=(70, 10))
-        platformGrid.Add(speed, pos=(1, 2))
+        # Processor
+        processorGrid = wx.GridBagSizer(hgap=5, vgap=3)
 
-        editspeed= wx.TextCtrl(self, pos=(70, 60), size=(70, -1))
-        platformGrid.Add(editspeed, pos=(2, 2))
+        platform = wx.StaticText(specpanel, label="Processors: ")
+        processorGrid.Add(platform, pos=(0, 0))
 
-        pButton1 = wx.Button(self, label="-")
-        pButton2 = wx.Button(self, label="+")
-        platformGrid.Add(pButton1, pos=(2, 3))
-        platformGrid.Add(pButton2, pos=(2, 4))
+        pname = wx.StaticText(specpanel, label="name: ")
+        processorGrid.Add(pname, pos=(1, 0))
 
-        # Mapping
-        mapGrid = wx.GridBagSizer(hgap=4, vgap=3)
+        edit_pname = wx.TextCtrl(specpanel, size=(70, -1))
+        processorGrid.Add(edit_pname, pos=(2, 0))
 
-        mapping = wx.StaticText(self, label="Mapping: ", pos=(10, 60))
-        mapGrid.Add(mapping, pos=(0, 0))
+        policy = wx.StaticText(specpanel, label="policy: ")
+        processorGrid.Add(policy, pos=(1, 1))
 
-        tsk_name = wx.StaticText(self, label="task name: ", pos=(40, 70))
-        mapGrid.Add(tsk_name, pos=(1, 0))
+        policylist = ['RMS', 'EDF']
+        policycombo = wx.ComboBox(specpanel, -1, size=(70, -1), value=" ", choices=policylist, style=wx.CB_DROPDOWN)
+        processorGrid.Add(policycombo, pos=(2, 1))
 
-        edit_tskname = wx.TextCtrl(self, pos=(10, 60), size=(70, -1))
-        mapGrid.Add(edit_tskname, pos=(2, 0))
+        preempt = wx.StaticText(specpanel, label="preemptible?: ")
+        processorGrid.Add(preempt, pos=(1, 2))
 
-        pt_name = wx.StaticText(self, label="pf name: ", pos=(40, 70))
-        mapGrid.Add(pt_name, pos=(1, 1))
+        preemptlist = ['true', 'false']
+        preemptcombo = wx.ComboBox(specpanel, -1, size=(70, -1), value=" ", choices=preemptlist, style=wx.CB_DROPDOWN)
+        processorGrid.Add(preemptcombo, pos=(2, 2))
 
-        editpfname = wx.TextCtrl(self, pos=(40, 60), size=(70, -1))
-        mapGrid.Add(editpfname, pos=(2, 1))
+        pro_dButton = wx.Button(specpanel, size=(70, -1), label="Delete")
+        pro_aButton = wx.Button(specpanel, size=(70, -1), label="Add")
+        processorGrid.Add(pro_dButton, pos=(2, 3))
+        processorGrid.Add(pro_aButton, pos=(2, 4))
 
-        mButton1 = wx.Button(self, label="-")
-        mButton2 = wx.Button(self, label="+")
-        mapGrid.Add(mButton1, pos=(2, 2))
-        mapGrid.Add(mButton2, pos=(2, 3))
+        # Bus
+        busgrid = wx.GridBagSizer(hgap=4, vgap=3)
+        bus = wx.StaticText(specpanel, label="Bus: ")
+        busgrid.Add(bus, pos=(0, 0))
 
-        # Denpendency
-        depGrid = wx.GridBagSizer(hgap=5, vgap=3)
-        dep = wx.StaticText(self, label="Dependency: ", pos=(0, 10))
-        depGrid.Add(dep, pos=(0, 0))
+        bus_bcet = wx.StaticText(specpanel, label="bcet")
+        busgrid.Add(bus_bcet, pos=(1, 0))
 
-        t_name = wx.StaticText(self, label="task name: ", pos=(10, 10))
-        depGrid.Add(t_name, pos=(1, 0))
+        edit_bus_bcet = wx.TextCtrl(specpanel, size=(70, -1))
+        busgrid.Add(edit_bus_bcet, pos=(2, 0))
 
-        edit_dtname = wx.TextCtrl(self, pos=(10, 60), size=(70, -1))
-        depGrid.Add(edit_dtname, pos=(2, 0))
+        bus_wcet = wx.StaticText(specpanel, label="wcet: ")
+        busgrid.Add(bus_wcet, pos=(1, 1))
 
-        predecessor = wx.StaticText(self, label="predecessor: ", pos=(40, 10))
-        depGrid.Add(predecessor, pos=(1, 1))
+        edit_bus_wcet = wx.TextCtrl(specpanel, size=(70, -1))
+        busgrid.Add(edit_bus_wcet, pos=(2, 1))
 
-        editpredecessor = wx.TextCtrl(self, pos=(40, 60), size=(70, -1))
-        depGrid.Add(editpredecessor, pos=(2, 1))
+        bus_dButton = wx.Button(specpanel,  size=(70, -1), label="Delete")
+        bus_aButton = wx.Button(specpanel, size=(70, -1),  label="Add")
+        busgrid.Add(bus_dButton, pos=(2, 2))
+        busgrid.Add(bus_aButton, pos=(2, 3))
 
-        data = wx.StaticText(self, label="data: ", pos=(70, 10))
-        depGrid.Add(data, pos=(1, 2))
+        modelgenbutton = wx.Button(specpanel, label='Model Generate!')
 
-        editdata= wx.TextCtrl(self, pos=(70, 60), size=(70, -1))
-        depGrid.Add(editdata, pos=(2, 2))
-
-        dButton1 = wx.Button(self, label="-")
-        dButton2 = wx.Button(self, label="+")
-        depGrid.Add(dButton1, pos=(2, 3))
-        depGrid.Add(dButton2, pos=(2, 4))
-
-        verifyButton = wx.Button(self, label='Verify!')
-
+        specSizer.Add(processorGrid, 0, wx.LEFT)
+        specSizer.Add(busgrid, 0, wx.LEFT)
         specSizer.Add(taskGrid, 0, wx.LEFT)
-        specSizer.Add(platformGrid, 0, wx.LEFT)
-        specSizer.Add(mapGrid, 0, wx.LEFT)
-        specSizer.Add(depGrid, 0, wx.LEFT)
-        specSizer.Add(verifyButton, 0, wx.LEFT)
+        specSizer.Add(modelgenbutton, 0, wx.LEFT)
+        specpanel.SetSizerAndFit(specSizer)
 
-        resultSizer = wx.BoxSizer(wx.VERTICAL)
-        result = wx.Panel(self, -1)
-        wx.TextCtrl(result, pos=(3, -1), size = (580, 250))
-        res = wx.StaticText(self, label='Verification Result:')
-        resultSizer.Add(res, 0, wx.LEFT)
-        resultSizer.Add(result, 0, wx.CENTER)
-
-        mainSizer.Add(specSizer, 0, wx.CENTER)
+        '''mainSizer.Add(specSizer, 0, wx.CENTER)
         mainSizer.Add(resultSizer, 0, wx.CENTER)
         self.SetSizerAndFit(mainSizer)
-        #self.add_task()
+        #self.add_task()'''
 
-    """def add_task(self):
-        sp = wx.Panel(self, -1)
-        self.mainSizer.Add(sp, 0, wx.CENTER)"""
-
+    '''def init_resultpenal(self):
+        resultSizer = wx.BoxSizer(wx.VERTICAL)
+        resultpanel = wx.Panel(self, -1)
+        wx.TextCtrl(resultpanel, pos=(3, -1), size = (580, 250))
+        res = wx.StaticText(self, label='Verification Result:')
+        resultSizer.Add(res, 0, wx.LEFT)
+        resultSizer.Add(resultpanel, 0, wx.CENTER)
+        resultpanel.SetSizerAndFit(resultSizer)'''
 
 def main():
     app = wx.App()
-    frame = mainFrame(None, 'Schedulability Analyse Tool')
+    frame = mainFrame(None, 'Real-time System Schedulability Analyse Tool')
     frame.Show()
     app.MainLoop()
 
