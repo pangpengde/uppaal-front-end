6275b781828c3e5d0603bb46afbda5d5b9e5feba
touyo <touyo.theone@gmail.com>
2014-12-21 15:00:16 +0800

    controller done

--- project/controller/Controller.py
+++ project/controller/Controller.py
@@ -14,21 +14,17 @@ class Controller(object):
     """docstring for Controller"""
 
     def __init__(self):
-        self.modelPath = '"../source/model.xml"'
-        self.propertyPath = '"../source/model.q"'
-        self.resultPath = '"../source/model.result"'
-        self.tracePath = '"../source/model.trace"'
-        self.verifytaPath = '../../uppaal-4.1.18/bin-Win32/verifyta.exe'
+        self.modelPath = r'..\source\model.xml'
+        self.propertyPath = r'..\source\model.q'
+        self.resultPath = r'..\source\model.result'
+        self.tracePath = r'..\source\model.trace'
+        self.verifytaPath = r'..\..\uppaal-4.1.18\bin-Win32\verifyta.exe'
 
     def model_validate(self):
          # 调用uppaal,验证model.xml,生成结果文件result.trace放在source文件夹下
         self.cmd = self.verifytaPath + ' -qst 1 ' + self.modelPath + ' ' + self.propertyPath \
                    + ' 1>' + self.resultPath + ' 2>' + self.tracePath
-        c = self.verifytaPath + ' -qst 1 ' + self.modelPath + ' ' + self.propertyPath
-        print self.cmd
         os.system(self.cmd)
-        print c
-        os.system(c)
 
 
 if __name__ == '__main__':
